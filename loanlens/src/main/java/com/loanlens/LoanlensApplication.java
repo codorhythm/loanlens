8be905ac3554a7e6fe06b36a3fa0de1c5312a199
Pratulya <pratulyapratap@gmail.com>
2026-06-09 17:32:30 +0530

    initial project setup

---
+++ loanlens/src/main/java/com/loanlens/LoanlensApplication.java
@@ -0,0 +1,13 @@
+package com.loanlens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanlensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanlensApplication.class, args);
+	}
+
+}
---
+++ loanlens/src/test/java/com/loanlens/LoanlensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loanlens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanlensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
